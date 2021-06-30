0c01bbc7db5c38db46317c22dc618cb1b8aa36cd
Ceheiss <cristobal.heiss.j@gmail.com>
2021-06-30 21:56:48 +0200

    outline basic plan

--- src/actions/index.js
+++ src/actions/index.js
@@ -19,3 +19,15 @@ export const setCurrentPage = (currentPage) => {
     payload: currentPage,
   };
 };
+
+// &channel=HOLIDU --> filter by channel
+// &_sort=score&_order=asc --> sort by score (asc or desc)
+
+/*
+  Channel I need to filter in the fetchReviews action because it is needed to build the pagination.
+  sorting by score in the fetchPageReviews because I want to sort what is shown
+
+  or
+
+  I call a different action with url and call the action on the selection. I will handle them in the same reducer
+*/
